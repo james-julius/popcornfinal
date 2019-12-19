a04ec85f3b59006d77ad636f18d34a90470a2efa
James <jamesjulius@Jamess-MacBook-Pro.local>
2019-12-19 18:08:59 +0000

    Made name smaller

---
+++ build/precache-manifest.d144bef1bf58ed4c0918923bfaf3c8e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb1370702b58308ee76648e1769203a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab666f9343b31ff654c",
+    "url": "/static/css/main.3bfecce2.chunk.css"
+  },
+  {
+    "revision": "80a2af948874d542448a",
+    "url": "/static/js/2.6c827685.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/static/js/2.6c827685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ab666f9343b31ff654c",
+    "url": "/static/js/main.96904d82.chunk.js"
+  },
+  {
+    "revision": "805605f639018974c6bd",
+    "url": "/static/js/runtime-main.cdca869d.js"
+  },
+  {
+    "revision": "2dae0ee6d81212459072cbfa417243e6",
+    "url": "/static/media/IMDb.2dae0ee6.png"
+  },
+  {
+    "revision": "34270fd3b03392355a36477682a8a980",
+    "url": "/static/media/MetaCritic.34270fd3.png"
+  },
+  {
+    "revision": "fc72d86241c4330f2d9f3435cfe0868b",
+    "url": "/static/media/film-background.fc72d862.png"
+  },
+  {
+    "revision": "ccb6952e2dad16de4dc33a98e4e622fa",
+    "url": "/static/media/loadingicon.ccb6952e.svg"
+  },
+  {
+    "revision": "ab9fd6bc8bcf0a7b1d1d81914c1f18e6",
+    "url": "/static/media/popcornsmooth.ab9fd6bc.png"
+  }
+]);
